792d5ac4fa15ad6bdf04417f5ff72b7db55ce1d9
vihangamanusha <vihangamanusha07@gmail.com>
2026-03-20 18:22:15 +0530

    update externalizable

---
+++ Externalizable/src/Vehical.java
@@ -0,0 +1,17 @@
+public class Vehical {
+    private String color;
+
+    public Vehical() {}
+
+    public Vehical(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
