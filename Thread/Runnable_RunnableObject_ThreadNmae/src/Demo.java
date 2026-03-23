28ebc9edd4ae0838e5541d7b5285f04cddc3a0ae
vihangamanusha <vihangamanusha07@gmail.com>
2026-03-23 17:25:22 +0530

    update

--- Thread/Runnable_RunnableObject_ThreadNmae/src/Demo.java
+++ Thread/Runnable_RunnableObject_ThreadNmae/src/Demo.java
@@ -1,15 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
-
-        MyRunnable r = new MyRunnable();   // Step 1: create Runnable object
-
-        Thread t = new Thread(r, "MyThread"); // Step 2: create Thread with name
-
-        t.start(); // Step 3: start thread
-
-        // Main thread work
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Main Thread : " + i);
-        }
+        MyThread myThread = new MyThread();
+        myThread.start();
     }
 }
