4258c89f0233aa2f0806a2650cb86666c673dc74
vihangamanusha <vihangamanusha07@gmail.com>
2026-03-24 10:13:29 +0530

    UPDATE

--- Thread/EX3/src/Demo.java
+++ Thread/EX3/src/Demo.java
@@ -1,6 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
         Thread  t=new Thread();
+        t.setPriority(6);
         System.out.println(t.getPriority());
         t.start();
     }
