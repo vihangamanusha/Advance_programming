5cafded56f41f57bbf0bf1684e5dbc8a6dd0e8be
vihangamanusha <vihangamanusha07@gmail.com>
2026-03-24 09:45:19 +0530

    folder new

--- Thread/EX2/src/MainDemo.java
+++ Thread/EX2/src/MainDemo.java
@@ -1,2 +1,18 @@
+import static java.lang.Character.getName;
+
 public class MainDemo {
+    public static void main(String[] args) {
+        Thread t1=new Thread(new ThreadExample(), "Thread1");
+        Thread t2=new Thread(new ThreadExample(), "Thread2");
+
+        /*System.out.println("starting "+t1.getName());
+        t1.run();
+        System.out.println("starting "+t2.getName());
+        t2.run();*/
+
+        System.out.println("starting "+t1.getName());
+        t1.start();
+        System.out.println("starting "+t2.getName());
+        t2.start();
+    }
 }
