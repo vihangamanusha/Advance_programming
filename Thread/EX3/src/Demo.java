8d8c9ecd03bf4a5b1201df768058b7b24ed689be
vihangamanusha <vihangamanusha07@gmail.com>
2026-03-24 12:25:55 +0530

    update

--- Thread/EX3/src/Demo.java
+++ Thread/EX3/src/Demo.java
@@ -3,7 +3,11 @@ public class Demo {
         TestMultiPriority t1 = new TestMultiPriority();
         TestMultiPriority t2 = new TestMultiPriority();
 
-        t1.setPriority(Th);
+        t1.setPriority(4);
+        t2.setPriority(Thread.MAX_PRIORITY);
+
+        t1.start();
+        t2.start();
 
     }
 }
