cb8da353b9ae59aa546c0dd0ae52998843ad18b9
vihangamanusha <vihangamanusha07@gmail.com>
2026-03-24 19:44:48 +0530

    update

--- Thread/EX4/src/Main.java
+++ Thread/EX4/src/Main.java
@@ -1,18 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        PrintNumbers p1 = new PrintNumbers();
-        PrintNumbers p2 = new PrintNumbers();
+
+        PrintNumbers obj1 = new PrintNumbers();
+        PrintNumbers obj2 = new PrintNumbers();
+
+        obj1.setName("Low Priority Thread");
+        obj2.setName("High Priority Thread");
 
 
-        p2.setName("Low priority");
-        p1.setName("High priority");
+        obj1.setPriority(Thread.MIN_PRIORITY);
+        obj2.setPriority(Thread.MAX_PRIORITY);
 
-
-        p2.setPriority(Thread.MIN_PRIORITY);
-        p1.setPriority(Thread.MAX_PRIORITY);
-
-
-        p2.start();
-        p1.start();
+        obj1.start();
+        obj2.start();
     }
 }
