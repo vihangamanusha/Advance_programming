1972badc061b31e058cb48a18797dbe87bf8010d
vihangamanusha <vihangamanusha07@gmail.com>
2026-03-25 15:26:19 +0530

    crete thread group mygroup

--- Thread/EX11/src/Demo.java
+++ Thread/EX11/src/Demo.java
@@ -1,15 +1,17 @@
-import java.lang.ThreadGroup;
+
 public class Demo {
     public static void main(String[] args) {
+        ThreadGroup tg = new ThreadGroup("Demo");
 
-        ThreadGroup tg = new ThreadGroup("MyGroup");
+        MyTask myTask = new MyTask();
 
-        Thread t1 = new Thread(tg, new MyTask(), "T1");
-        Thread t2 = new Thread(tg, new MyTask(), "T2");
+        Thread t1 = new Thread(tg, myTask,"Thread1");
+        Thread t2 = new Thread(tg, myTask,"Thread2");
 
         t1.start();
         t2.start();
 
-        System.out.println("Group Name: " + tg.getName());
+        System.out.println(tg.getName()+" running");
+
     }
 }
