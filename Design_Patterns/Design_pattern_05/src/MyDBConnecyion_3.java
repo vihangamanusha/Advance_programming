f361f6d8f94c9a4cc66d2291d8ca8051833bca3d
vihangamanusha <vihangamanusha07@gmail.com>
2026-03-26 02:00:23 +0530

    update MyDBConnecyion class

--- Design_Patterns/Design_pattern_05/src/MyDBConnecyion_3.java
+++ Design_Patterns/Design_pattern_05/src/MyDBConnecyion_3.java
@@ -1,2 +1,13 @@
 public class MyDBConnecyion_3 {
+    private static MyDBConnecyion_3 mydb3;
+
+    private MyDBConnecyion_3(){
+
+    }
+    public static MyDBConnecyion_3 getInstance(){
+        if(mydb3==null){
+            mydb3 = new MyDBConnecyion_3();
+        }
+        return mydb3;
+    }
 }
