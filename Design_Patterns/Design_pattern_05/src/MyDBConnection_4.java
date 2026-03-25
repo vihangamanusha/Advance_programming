93afb0e85c7a82259a9b2906a92d0e14be4a729c
vihangamanusha <vihangamanusha07@gmail.com>
2026-03-26 02:45:00 +0530

    create main class

---
+++ Design_Patterns/Design_pattern_05/src/MyDBConnection_4.java
@@ -0,0 +1,13 @@
+public class MyDBConnection_4 {
+    private static MyDBConnection_4 mydb4;
+
+    private MyDBConnection_4() {
+
+    }
+    public static synchronized MyDBConnection_4 getInstance() {
+        if(mydb4==null){
+            mydb4 = new MyDBConnection_4();
+        }
+        return mydb4;
+    }
+}
