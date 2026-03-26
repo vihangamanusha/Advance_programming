d3ba5cbebeddb8418e016bf0ce6d86c20922f313
vihangamanusha <vihangamanusha07@gmail.com>
2026-03-26 10:00:58 +0530

    delete class

--- Design_Patterns/Factory/src/MyDBConnection_1.java
+++
@@ -1,9 +0,0 @@
-public class MyDBConnection_1 {
-    private static MyDBConnection_1 mydb1=new MyDBConnection_1();
-
-    private MyDBConnection_1(){}
-
-    public static MyDBConnection_1 getInstance(){
-        return mydb1;
-    }
-}
