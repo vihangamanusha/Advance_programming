6a52960790b25456d7aaa6ff1b43cac1542e8c8f
vihangamanusha <vihangamanusha07@gmail.com>
2026-03-27 00:51:43 +0530

    delete

--- Thread/ImplementRUNNABLE/src/ThreadDemo.java
+++
@@ -1,7 +0,0 @@
-public class ThreadDemo {
-    public static void main(String[] args) {
-        MyThread myThread = new MyThread();
-        Thread thread = new Thread(myThread);
-        thread.start();
-    }
-}
--- Thread/Runnable_RunnableObject_ThreadNmae/src/Demo.java
+++
@@ -1,9 +0,0 @@
-public class Demo {
-    public static void main(String[] args) throws InterruptedException {
-        MyRunnable myRunnable = new MyRunnable();
-        Thread thread = new Thread(myRunnable,"MyRunnable");
-        thread.start();
-        thread.join();
-        System.out.println("Main thread exiting");
-    }
-}
