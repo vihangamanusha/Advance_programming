b375261ff64a5cdf75b08aa627a2a77f019fcf13
vihangamanusha <vihangamanusha07@gmail.com>
2026-03-26 11:07:15 +0530

    update

---
+++ Design_Patterns/Abstract_Factory/src/FactoryProducer.java
@@ -0,0 +1,11 @@
+public class FactoryProducer {
+    public static AbstractFactory getFactory(String type) {
+        if (type.equalsIgnoreCase("shape")) {
+            return new ShapeFactory();
+        }else if (type.equalsIgnoreCase("color")) {
+            return new ColorFactory();
+        }return null;
+
+    }
+
+}
