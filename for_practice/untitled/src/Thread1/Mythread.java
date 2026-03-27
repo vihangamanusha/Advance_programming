0a2f28ed121bcfdd99a0e2aea7f27b4a456f19a2
vihangamanusha <vihangamanusha07@gmail.com>
2026-03-27 07:45:06 +0530

    update

---
+++ for_practice/untitled/src/Thread1/Mythread.java
@@ -0,0 +1,9 @@
+package Thread1;
+
+public class Mythread extends Thread {
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+}
