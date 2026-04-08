dbcfda9c50fbbcd97fb4953648c3e95ce229f155
vihangamanusha <vihangamanusha07@gmail.com>
2026-04-08 22:08:19 +0530

    updte

--- for_practice/src/UDP/Client.java
+++ for_practice/src/UDP/Client.java
@@ -1,21 +1,18 @@
 package UDP;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.io.IOException;
+import java.net.*;
 
 public class Client {
     public static void main(String[] args) {
         try {
-            DatagramSocket socket = new DatagramSocket();
+            DatagramSocket ds = new DatagramSocket();
             String str="welcome a";
             InetAddress address = InetAddress.getByName("127.0.0.1");
             DatagramPacket dp=new DatagramPacket(str.getBytes(), str.length(), address, 1234);
-
             ds.send(dp);
             ds.close();
-        } catch (SocketException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
         }
