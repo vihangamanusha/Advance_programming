f2f67625121e490e426584ad53a2d2597068afaf
vihangamanusha <vihangamanusha07@gmail.com>
2026-04-08 15:16:16 +0530

    update

--- for_practice/src/MyClass.java
+++
@@ -1,9 +0,0 @@
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-public class MyClass {
-    public static void main(String[] args) throws UnknownHostException {
-        InetAddress ip=InetAddress.getByName("facebook.com");
-        System.out.println(ip);
-    }
-}
