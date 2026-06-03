dbe90b79bd344974784e96039b7d6177c111962d
vihangamanusha <vihangamanusha07@gmail.com>
2026-06-04 02:01:23 +0530

    update

--- new/untitled/src/Serialization/EX_01/MyDeserializer.java
+++ new/untitled/src/Serialization/EX_01/MyDeserializer.java
@@ -1,4 +1,22 @@
 package Serialization.EX_01;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
 public class MyDeserializer {
+    public static void main(String[] args) {
+        try{
+            FileInputStream fis=new FileInputStream("C:\\student\\student.ser");
+            ObjectInputStream ois=new ObjectInputStream(fis);
+            Student student=(Student)ois.readObject();
+            System.out.println(student.getId());
+            System.out.println(student.getName());
+            System.out.println(student.getAge());
+            ois.close();
+            fis.close();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
