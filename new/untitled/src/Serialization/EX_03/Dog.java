652afddbb28689f48e18287f8222b2da7b15944c
vihangamanusha <vihangamanusha07@gmail.com>
2026-06-04 02:26:08 +0530

    complete dog.java

--- new/untitled/src/Serialization/EX_03/Dog.java
+++ new/untitled/src/Serialization/EX_03/Dog.java
@@ -1,4 +1,23 @@
 package Serialization.EX_03;
 
-public class Dog {
+public class Dog extends Animal {
+    private String color;
+
+    public Dog (double weight, String color) {
+        super(weight);
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog [color=" + color + "weight="+getWeight() +" ";
+    }
 }
