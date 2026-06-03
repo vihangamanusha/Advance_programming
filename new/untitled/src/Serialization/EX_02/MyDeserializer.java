cda780c1bf03c31ad61d5d12b53c7749dafbf055
vihangamanusha <vihangamanusha07@gmail.com>
2026-06-04 02:15:34 +0530

    update

--- new/untitled/src/Serialization/EX_02/MyDeserializer.java
+++ new/untitled/src/Serialization/EX_02/MyDeserializer.java
@@ -1,4 +1,17 @@
 package Serialization.EX_02;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
 public class MyDeserializer {
+    public static void main(String[] args) {
+        try{
+            FileInputStream fis=new FileInputStream("C:\\student\\student.ser");
+            ObjectInputStream ois=new ObjectInputStream(fis);
+            Student stu=(Student)ois.readObject();
+            System.out.println(stu.id+" "+stu.name);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- new/untitled/src/Serialization/EX_02/MySerializer.java
+++ new/untitled/src/Serialization/EX_02/MySerializer.java
@@ -1,4 +1,23 @@
 package Serialization.EX_02;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 public class MySerializer {
+    public static void main(String[] args) {
+        Student stu=new Student();
+        stu.id=1;
+        stu.name="manudha";
+
+        try{
+            FileOutputStream fos=new FileOutputStream("C:\\student\\student.ser");
+            ObjectOutputStream oos=new ObjectOutputStream(fos);
+            oos.writeObject(stu);
+            oos.flush();
+            oos.close();
+            fos.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
