bfa0ee83b020c9a157f6af456c556f74902d6112
vihangamanusha <vihangamanusha07@gmail.com>
2026-06-04 06:24:51 +0530

    create employee

---
+++ new/untitled/src/Serialization/EX_04/Employee.java
@@ -0,0 +1,28 @@
+package Serialization.EX_04;
+
+public class Employee {
+    private String name;
+    private int age;
+
+    public Employee(){}
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
