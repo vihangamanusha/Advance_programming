2eb1b2bdc791884d306c222edeed4fb81d509e01
vihangamanusha <vihangamanusha07@gmail.com>
2026-06-04 06:28:58 +0530

    update uniemployee

--- new/untitled/src/Serialization/EX_04/UniEmployee.java
+++ new/untitled/src/Serialization/EX_04/UniEmployee.java
@@ -1,4 +1,20 @@
 package Serialization.EX_04;
 
-public class UniEmployee {
+import java.io.Serializable;
+
+public class UniEmployee extends Employee implements Serializable {
+    private int uniId;
+
+    public UniEmployee(String name,int age,int uniId) {
+        super(name,age);
+        this.uniId = uniId;
+    }
+
+    public int getUniId() {
+        return uniId;
+    }
+
+    public void setUniId(int uniId) {
+        this.uniId = uniId;
+    }
 }
