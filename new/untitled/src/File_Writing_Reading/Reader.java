80c42aa7227dc38b40a27ef3033a203699827785
vihangamanusha <vihangamanusha07@gmail.com>
2026-06-05 06:40:02 +0530

    update

--- new/untitled/src/File_Writing_Reading/Reader.java
+++ new/untitled/src/File_Writing_Reading/Reader.java
@@ -1,2 +1,20 @@
 package File_Writing_Reading;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Reader {
+    public static void main(String[] args) {
+        try{
+            BufferedReader reader=new BufferedReader(new FileReader("students.txt"));
+
+            String line;
+
+            while((line=reader.readLine())!=null){
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
