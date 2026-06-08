3c7ac0d6fe4182d8a026846e56e97157d391ac9c
vihangamanusha <vihangamanusha07@gmail.com>
2026-06-08 21:21:56 +0530

    update

--- new/untitled/src/Thread/Extending_thread_class/MyThread.java
+++ new/untitled/src/Thread/Extending_thread_class/MyThread.java
@@ -1,4 +1,7 @@
 package Thread.Extending_thread_class;
 
-public class MyThread {
+public class MyThread extends Thread {
+    public void run() {
+        System.out.println("MyThread");
+    }
 }
--- new/untitled/src/Thread/Extending_thread_class/ThreadDemo.java
+++ new/untitled/src/Thread/Extending_thread_class/ThreadDemo.java
@@ -1,4 +1,8 @@
 package Thread.Extending_thread_class;
 
 public class ThreadDemo {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+    }
 }
