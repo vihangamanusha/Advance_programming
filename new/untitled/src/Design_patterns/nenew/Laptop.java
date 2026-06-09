71cd0e3b73cb3b39fbbfc78912cc093cffc10686
vihangamanusha <vihangamanusha07@gmail.com>
2026-06-10 00:44:27 +0530

    delete

--- new/untitled/src/Design_patterns/nenew/Laptop.java
+++
@@ -1,9 +0,0 @@
-package Design_patterns.nenew;
-
-public class Laptop {
-      public Laptop() {}
-
-      public String showDescription() {
-          return "Laptop";
-      }
-}
--- new/untitled/src/Design_patterns/nenew/LaptopDecorator.java
+++
@@ -1,5 +0,0 @@
-package Design_patterns.nenew;
-
-public abstract class LaptopDecorator extends Laptop {
-    public abstract String showDescription();
-}
