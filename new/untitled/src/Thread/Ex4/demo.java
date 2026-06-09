e9a73aada36b8816bd1f8f6cd791314db4312e84
vihangamanusha <vihangamanusha07@gmail.com>
2026-06-09 07:04:18 +0530

    update

--- new/untitled/src/Thread/Ex4/demo.java
+++ new/untitled/src/Thread/Ex4/demo.java
@@ -1,4 +1,17 @@
 package Thread.Ex4;
 
 public class demo {
+    public static void main(String[] args) {
+        printnumber thread1 = new printnumber();
+        printnumber thread2 = new printnumber();
+
+        thread1.setName("low priority thread");
+        thread2.setName("high priority thread");
+
+        thread1.setPriority(Thread.MIN_PRIORITY);
+        thread2.setPriority(Thread.MAX_PRIORITY);
+
+        thread1.start();
+        thread2.start();
+    }
 }
--- new/untitled/src/Thread/Ex4/printnumber.java
+++ new/untitled/src/Thread/Ex4/printnumber.java
@@ -1,4 +1,15 @@
 package Thread.Ex4;
 
-public class printnumber {
+public class printnumber extends Thread {
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + ": " + i);
+            Thread.yield();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
