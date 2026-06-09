5042f4231b15bea0048517f1eb50ec8ae851823c
vihangamanusha <vihangamanusha07@gmail.com>
2026-06-09 20:01:11 +0530

    safe

--- new/untitled/src/Design_patterns/Singleton_types/MyDBConnection_4.java
+++ new/untitled/src/Design_patterns/Singleton_types/MyDBConnection_4.java
@@ -1,4 +1,14 @@
 package Design_patterns.Singleton_types;
 
 public class MyDBConnection_4 {
+    private static MyDBConnection_4 mydb4;
+
+    private MyDBConnection_4() {}
+
+    public static synchronized MyDBConnection_4 getInstance() {
+        if (mydb4==null) {
+            mydb4=new MyDBConnection_4();
+        }
+        return mydb4;
+    }
 }
