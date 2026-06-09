b3d9253eb54d85e6957c6117ce9899013f1e41b2
vihangamanusha <vihangamanusha07@gmail.com>
2026-06-09 19:50:12 +0530

    update

--- new/untitled/src/Design_patterns/Singleton_types/MyDBConnection_3.java
+++ new/untitled/src/Design_patterns/Singleton_types/MyDBConnection_3.java
@@ -1,5 +1,14 @@
 package Design_patterns.Singleton_types;
 
 public class MyDBConnection_3 {
-    
+    private static MyDBConnection_3 mydb3;
+
+    private MyDBConnection_3() {}
+
+    public static MyDBConnection_3 getInstance() {
+        if(mydb3==null) {
+            mydb3=new MyDBConnection_3();
+        }
+        return mydb3;
+    }
 }
---
+++ new/untitled/src/Design_patterns/Singleton_types/MyDBConnection_4.java
@@ -0,0 +1,4 @@
+package Design_patterns.Singleton_types;
+
+public class MyDBConnection_4 {
+}
