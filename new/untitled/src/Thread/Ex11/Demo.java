bec701c31b3150c6b3c1437f270f00883d21e824
vihangamanusha <vihangamanusha07@gmail.com>
2026-06-09 10:29:53 +0530

    update

--- new/untitled/src/Thread/Ex11/Demo.java
+++ new/untitled/src/Thread/Ex11/Demo.java
@@ -2,6 +2,15 @@ package Thread.Ex11;
 
 public class Demo {
     public static void main(String[] args) {
-        
+        ThreadGroup group = new ThreadGroup("ThreadGroup");
+
+        Mytask mytask = new Mytask();
+        Thread t1=new Thread(group,mytask,"Thread 1");
+        Thread t2=new Thread(group,mytask,"Thread 2");
+
+        t1.start();
+        t2.start();
+
+        System.out.println(group.getName());
     }
 }
--- new/untitled/src/Thread/Ex11/Mytask.java
+++ new/untitled/src/Thread/Ex11/Mytask.java
@@ -2,6 +2,6 @@ package Thread.Ex11;
 
 public class Mytask implements Runnable {
     public void run() {
-
+        System.out.println(Thread.currentThread().getName());
     }
 }
